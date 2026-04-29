11e790e7a47523fdc81275c6fcd74f68b5012ec8
yardas-dev <selector.yardas_0u@icloud.com>
2026-04-29 19:18:07 +0200

    Añadir importación directamente y eliminar del head HTML

--- src/componente-base.js
+++ src/componente-base.js
@@ -1,9 +1,7 @@
 import { LitElement, css } from "lit"
 
-const estilos = css`
-    @import url('https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css');
-`
-
 export class ComponenteBase extends LitElement {
-    static styles = [estilos]
+    static styles = css`
+        @import url('https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css');
+    `
 }
