5056a961b9e92a6930588b412df180f7d2648ca4
yardas-dev <selector.yardas_0u@icloud.com>
2026-04-29 18:58:10 +0200

    Código de prueba

--- src/poke-app.js
+++ src/poke-app.js
@@ -2,7 +2,12 @@ import { LitElement, html } from "lit"
 
 class PokeApp extends LitElement {
     render() {
-        return html`<main>Aventura en la región de Sinnoh</main>`
+        return html`
+        <main style="display: block">
+            <p class="title">Aventura en la región de Sinnoh</p>
+            <p class="subtitle is-2">Por YardasDev</p>
+        </main>
+        `
     }
 }
 customElements.define("poke-app", PokeApp)
