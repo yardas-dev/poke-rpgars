fd8cb7777e5d0e48240bd707db536cb893d7b5dc
yardas-dev <selector.yardas_0u@icloud.com>
2026-04-29 19:23:02 +0200

    Añadir unsafeCSS

--- src/componente-base.js
+++ src/componente-base.js
@@ -1,7 +1,7 @@
-import { LitElement, css } from "lit"
+import { LitElement, css, unsafeCSS } from "lit"
 
 export class ComponenteBase extends LitElement {
-    static styles = css`
-        @import url('https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css');
-    `
+    static styles = [
+        css`${unsafeCSS(`@import url('https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css');`)}`
+    ]
 }
