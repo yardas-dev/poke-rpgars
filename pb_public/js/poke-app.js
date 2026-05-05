7b5a691e75a0ae3de64ff641abef4e371c5fac79
yardas-dev <selector.yardas_0u@icloud.com>
2026-05-05 22:03:26 +0200

    Extender PokeApp de ComponenteBase en vez de LitElement

--- pb_public/js/poke-app.js
+++ pb_public/js/poke-app.js
@@ -1,6 +1,7 @@
 import { LitElement, html } from "https://cdn.jsdelivr.net/npm/lit@3.3.2/+esm"
+import ComponenteBase from "./components/componente-base.js"
 
-class PokeApp extends LitElement {
+class PokeApp extends ComponenteBase {
     render() {
         return html`
             <p>Barra de navegación</p>
