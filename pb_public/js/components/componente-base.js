4295e9c508aab4249ca53f060f2a406c6d76c3a4
yardas-dev <selector.yardas_0u@icloud.com>
2026-05-05 22:00:17 +0200

    Crear componente base con estilos Bulma

---
+++ pb_public/js/components/componente-base.js
@@ -0,0 +1,7 @@
+import { LitElement, css } from "https://cdn.jsdelivr.net/npm/lit@3.3.2/+esm"
+
+export class ComponenteBase extends LitElement {
+    static styles = css`
+        @import url('https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css');
+    `
+}
