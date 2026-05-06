e83b3812c94ecf58958a30cffc44e99bfca48702
yardas-dev <selector.yardas_0u@icloud.com>
2026-05-06 16:25:25 +0200

    Añadir ficha de personajes inacabada y dependencia SDK PocketBase

--- src/componentes/componente-base.js
+++ src/componentes/componente-base.js
@@ -1,8 +1,59 @@
 import { LitElement } from "lit"
+import PocketBase from "pocketbase"
+
+const ORIGEN = "http://localhost:8090"
 
 export class ComponenteBase extends LitElement {
     createRenderRoot() {
         return this
+    }
+
+    static get properties() {
+        return {
+            naturalezas: { type: Array },
+            rangos: { type: Array },
+            personajes: { type: Array },
+        }
+    }
+
+    constructor() {
+        super()
+        this.naturalezas = []
+        this.rangos = []
+        this.personajes = []
+    }
+
+    async connectedCallback() {
+        super.connectedCallback()
+
+        const pb = new PocketBase(ORIGEN)
+
+        const naturalezas = await pb.collection("naturalezas").getFullList()
+        const rangos = await pb.collection("rangos").getFullList()
+        const personajes = await pb.collection("personajes").getFullList()
+
+        this.naturalezas = naturalezas.map(naturaleza => ({
+            id: naturaleza.id,
+            nombre: naturaleza.nombre,
+        }))
+        this.rangos = rangos.map(rango => ({
+            id: rango.id,
+            icono: this._obtenerRutaArchivo(rango, "icono"),
+            nombre: rango.nombre,
+        }))
+        this.personajes = personajes.map(personaje => ({
+            icono: this._obtenerRutaArchivo(personaje, "icono"),
+            nombre: personaje.nombre,
+            sexo: personaje.sexo,
+            rol: personaje.rol,
+            ocupacion: personaje.ocupacion,
+            concepto: personaje.concepto,
+            edad: personaje.edad,
+            naturaleza: this.naturalezas.find(
+                naturaleza => naturaleza.id === personaje.naturaleza
+            ),
+            rango: this.rangos.find(rango => rango.id === personaje.rango),
+        }))
     }
 
     _capturarEnlace(ev) {
@@ -21,5 +72,16 @@ export class ComponenteBase extends LitElement {
             bubbles: true,
             composed: true
         }))
+    }
+
+    _normalizarMayús(texto) {
+        return texto[0].toUpperCase() + texto.slice(1)
+    }
+
+    _obtenerRutaArchivo(registro, propiedad) {
+        return `${ORIGEN}/api/files/` +
+            `${registro.collectionId}/` +
+            `${registro.id}/` +
+            registro[propiedad]
     }
 }
