94458e41d2ed28b23c8e6c5260a19d6ad55dc388
yardas-dev <selector.yardas_0u@icloud.com>
2026-05-07 15:27:24 +0200

    Añadir barra de navegación

---
+++ pb_public/js/components/barra-navegacion.js
@@ -0,0 +1,45 @@
+import { html } from "https://cdn.jsdelivr.net/npm/lit@3.3.2/+esm"
+import { ComponenteBase } from "./componente-base.js"
+
+export class BarraNavegacion extends ComponenteBase {
+    render() {
+        const items = [
+            { ruta: "/", texto: "Personajes" },
+            { ruta: "/equipos", texto: "Equipos" },
+            { ruta: "/combates", texto: "Combates" },
+            { ruta: "/encuentros", texto: "Encuentros" },
+            { ruta: "/pokedex", texto: "Pokédex" },
+        ]
+
+        return html`
+            <nav class="navbar">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="/">
+                        Imagen aún no disponible
+                    </a>
+
+                    <a role="button" class="navbar-burger">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div class="navbar-menu">
+                    <div class="navbar-start">
+                        ${
+                            items.map(
+                                item => html`
+                                    <a class="navbar-item" href=${item.ruta}>
+                                        ${item.texto}
+                                    </a>
+                                `
+                            )
+                        }
+                </div>
+            </nav>
+        `
+    }
+}
+customElements.define("barra-navegacion", BarraNavegacion)
--- pb_public/js/poke-app.js
+++ pb_public/js/poke-app.js
@@ -1,10 +1,11 @@
 import { LitElement, html } from "https://cdn.jsdelivr.net/npm/lit@3.3.2/+esm"
 import { ComponenteBase } from "./components/componente-base.js"
+import "./components/barra-navegacion.js"
 
 class PokeApp extends ComponenteBase {
     render() {
         return html`
-            <p>Barra de navegación</p>
+            <barra-navegacion></barra-navegacion>
             <p class="title">Vista actual</p>
             <p>Contenido…</p>
         `
