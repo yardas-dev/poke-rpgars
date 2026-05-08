3b6f544ce054d44c1c3c7c84e1528f37c230b832
yardas-dev <selector.yardas_0u@icloud.com>
2026-05-08 12:30:43 +0200

    Restaurar desarrollo de las fichas de personajes

--- pb_public/js/components/componente-base.js
+++ pb_public/js/components/componente-base.js
@@ -4,4 +4,15 @@ export class ComponenteBase extends LitElement {
     createRenderRoot() {
         return this
     }
+
+    _normalizarMayus(texto) {
+        return texto[0].toUpperCase() + texto.slice(1)
+    }
+
+    _obtenerRutaArchivo(registro, propiedad) {
+        return "/api/files/" +
+            `${registro.collectionId}/` +
+            `${registro.id}/` +
+            registro[propiedad]
+    }
 }
--- pb_public/js/views/vista-personajes.js
+++ pb_public/js/views/vista-personajes.js
@@ -1,9 +1,67 @@
 import { html } from "https://cdn.jsdelivr.net/npm/lit@3.3.2/+esm"
+import { cargar } from "../lib/pocketbase.js"
 import { ComponenteBase } from "../components/componente-base.js"
+import "../components/ficha-personaje.js"
 
 export class VistaPersonajes extends ComponenteBase {
+    static get properties() {
+        return {
+            personajes: { type: Array }
+        }
+    }
+
+    constructor() {
+        super()
+        this.personajes = []
+    }
+
+    async connectedCallback() {
+        super.connectedCallback()
+        this.personajes = await this._traerPersonajes()
+    }
+
+    async _traerPersonajes() {
+        const pb = cargar()
+        const personajes = await pb.collection("personajes").getFullList()
+        const naturalezas = await pb.collection("naturalezas").getFullList()
+        const rangos = await pb.collection("rangos").getFullList()
+
+        personajes.forEach(personaje => {
+            personaje.icono = this._obtenerRutaArchivo(personaje, "icono")
+            personaje.naturaleza = naturalezas.find(
+                naturaleza => naturaleza.id === personaje.naturaleza
+            )
+            personaje.rango = rangos.find(rango => rango.id === personaje.rango)
+            personaje.rango.icono = this._obtenerRutaArchivo(
+                personaje.rango, "icono"
+            )
+        })
+
+        return personajes
+    }
+
     render() {
-        return html`<p class="title">Personajes</p>`
+        return html`
+            <p class="title">
+                Personajes
+            </p>
+
+            <div class="columns">
+                ${
+                    this.personajes.filter(
+                        personaje => personaje.rol === "jugador"
+                    ).map(
+                        personaje => html`
+                            <div class="column">
+                                <ficha-personaje
+                                    .personaje=${personaje}
+                                ></ficha-personaje>
+                            </div>
+                        `
+                    )
+                }
+            </div>
+        `
     }
 }
 customElements.define("vista-personajes", VistaPersonajes)
