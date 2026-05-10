998eae2dfa6bab270ff3a5fb2d8a84503d873c59
yardas-dev <selector.yardas_0u@icloud.com>
2026-05-10 13:26:23 +0200

    Avanzar desarrollo general de ficha de personaje

--- pb_public/js/components/caja-informativa.js
+++ pb_public/js/components/caja-informativa.js
@@ -4,26 +4,110 @@ import { ComponenteBase } from "./componente-base.js"
 class CajaInformativa extends ComponenteBase {
     static get properties() {
         return {
+            tipo: { type: String },
             titulo: { type: String },
-            texto: { type: String },
+            valor: { type: String },
+            estaHecho: { type: Boolean },
+            color: { type: String },
+            margenY: { type: Number },
         }
     }
 
     constructor() {
         super()
-        this.titulo, this.texto = ""
+        this.tipo, this.titulo, this.valor, this.color = ""
+        this.estaHecho = false
+    }
+
+    renderizarTexto() {
+        return html`
+            <p class="subtitle is-6">
+                ${this.valor}
+            </p>
+        `
+    }
+
+    renderizarValorVacio() {
+        return html`
+            <p class="subtitle is-6 has-text-dark-soft">
+                —
+            </p>
+        `
+    }
+
+    renderizarPuntos() {
+        let veces = Number(this.valor)
+        let serieVacia = Array(veces).fill(null)
+        let circulo = html`
+            <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
+                <circle
+                    style="fill: var(--bulma-subtitle-color)"
+                    r="50%"
+                    cx="50%"
+                    cy="50%"
+                />
+            </svg>
+        `
+
+        return html`
+            <p class="subtitle is-6">
+                ${serieVacia.map(() => circulo)}
+            </p>
+        `
+    }
+
+    renderizarLogro() {
+        let clasesAdicionales = this.estaHecho ? "" : "has-text-dark-soft"
+        
+        return html`
+            <p class="subtitle is-6 ${clasesAdicionales}">
+                ${this.valor} ${this.estaHecho ? "✅" : ""}
+            </p>
+        `
     }
 
     render() {
+        let tituloEstaApagado = false
+        let parrafoValor
+
+        switch (this.tipo) {
+            case "texto":
+                parrafoValor = this.renderizarTexto()
+                break
+
+            case "puntos":
+                let tieneCeroPuntos = Number(this.valor) === 0
+                tituloEstaApagado = tieneCeroPuntos
+                parrafoValor = tieneCeroPuntos
+                    ? this.renderizarValorVacio()
+                    : this.renderizarPuntos()
+                break
+
+            case "logro":
+                tituloEstaApagado = ! this.estaHecho
+                parrafoValor = this.renderizarLogro()
+                break
+
+            case "html":
+                parrafoValor = html`<div><slot></slot></div>`
+
+            default:
+                break
+        }
+
+        let clasesAdicionales = `has-background-${this.color}-soft ` +
+            (this.margenY === undefined ? "" : `my-${this.margenY}`)
+        let clasesTituloAdicionales = tituloEstaApagado
+            ? "has-text-dark-soft"
+            : ""
+
         return html`
-            <div class="box">
-                <p class="title is-5">
+            <div class="box ${clasesAdicionales}">
+                <p class="title is-5 ${clasesTituloAdicionales}">
                     ${this.titulo}
                 </p>
 
-                <p class="subtitle is-6">
-                    ${this.texto}
-                </p>
+                ${parrafoValor}
             </div>
         `
     }
